0d6d4b44137d4490bb94b0a50f09078fa46aa27e
Clarissa Forbes <53020872+caforbes@users.noreply.github.com>
2024-08-10 00:15:22 -0700

    better flask shell experience

--- my750.py
+++ my750.py
@@ -1,4 +1,12 @@
-from app import app
 from dotenv import load_dotenv
 
+from app import app
+from app.db import dbconnect
+from app.models import Entry
+
 load_dotenv()
+
+
+@app.shell_context_processor
+def make_shell_context():
+    return {"dbconnect": dbconnect, "Entry": Entry}
